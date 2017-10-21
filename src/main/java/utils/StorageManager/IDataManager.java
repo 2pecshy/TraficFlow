bec39de10ebc2f352fb502e586a1ae0f0b8847ce
Jeremy Lefebvre <jeremy.lefebvre@etu.unice.fr>
2017-10-21 22:45:59 +0200

    +DataManager   + Méthode qui lit les infos qui faut (NoeudSrc, NoeudEnd, nbVoies) depuis un fichier. Separator ","

--- src/main/java/utils/StorageManager/IDataManager.java
+++ src/main/java/utils/StorageManager/IDataManager.java
@@ -1,16 +1,19 @@
-package utils;
+package utils.StorageManager;
 
 import utils.Map.Map;
 import utils.Stat.Stat;
 
+import java.io.File;
+
 /**
  * Created by Matthieu on 14/10/2017.
  */
-public interface DataManager {
+public interface IDataManager {
 
     //mode = (web, file, BDD etc)
 
     public Map loadMap(String mode, String path);
+    public Map loadMap(String mode, File file);
 
     public void saveMap(String mode, String path);
 
