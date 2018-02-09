5d783198b1ca4ccf8222b5676f42b8e767b98ca3
MatthieuGenovese <gm102445@etu.unice.fr>
2018-02-09 21:22:05 +0100

    Bonsoir

---
+++ webconfig/src/test/java/WebConfigIntegrationTest.java
@@ -0,0 +1,64 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import sample.SimulationWebConfiguration;
+import services.configuration.WebConfigurationService;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = WebConfigurationService.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+@EnableBinding(Source.class)
+public class WebConfigIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private Source channels;
+
+    @Autowired
+    private MessageCollector collector;
+
+
+
+    private String jsonBody = "{" +
+            " \"simulationLength\" : 1," +
+            " \"simulationStart\" : 3, " +
+            "\"HOVLanes\" : \"true\", " +
+            "\"migrationPendulaire\" : \"False\", " +
+            "\"mapLink\" : \"https://api.openstreetmap.org/api/0.6/map?bbox=11.54,48.14,11.543,48.145\" " +
+            "}";
+
+    private JSONObject jsonObject = new JSONObject(jsonBody);
+    private SimulationWebConfiguration goodWebConfig = new SimulationWebConfiguration(jsonObject);
+
+    public WebConfigIntegrationTest() throws JSONException {
+    }
+
+
+    @Test
+    public void postMethodShouldReturnGoodWebConfig() throws InterruptedException {
+
+        SimulationWebConfiguration test = (SimulationWebConfiguration) this.restTemplate.postForObject(
+                "http://localhost:" + port + "/config",
+                goodWebConfig,
+                SimulationWebConfiguration.class);
+        //assertThat(messages, receivesPayloadThat(is(goodWebConfig)));
+        assertEquals(test.getSimulationLenght(), goodWebConfig.getSimulationLenght());
+    }
+
+}
